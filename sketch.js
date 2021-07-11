80bd9415c2e315b5f1cbaeb43b7b01abc40ea7c3
trick210 <trick210@live.com>
2021-07-12 01:48:15 +0200

    added background image

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
+let bg;
+
 function setup() {
+  bg = loadImage('assets/background.png');
   createCanvas(400, 400);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+
+  background(bg);
 }
