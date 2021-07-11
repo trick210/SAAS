900dae441136b1a320d2504385373b6edf66c2e9
trick210 <trick210@live.com>
2021-07-12 01:41:48 +0200

    test1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-  // put setup code here
+  createCanvas(400, 400);
 }
 
 function draw() {
-  // put drawing code here
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
