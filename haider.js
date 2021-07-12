05997bb2ee5340a7e525ced8abf1b9cf86ca467c
trick210 <trick210@live.com>
2021-07-12 02:29:07 +0200

    Update haider.js

--- haider.js
+++ haider.js
@@ -4,7 +4,7 @@ class Haider {
   constructor(posX, posY) {
     this.x = posX;
     this.y = posY;
-    this.img = loadImage('assets/haider.png');
+    this.img = loadImage('assets/Haider.png');
   }
 
   move() {
